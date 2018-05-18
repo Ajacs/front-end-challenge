4932db7b1e10f52cf58a60a80f4fd323cb0aef19
adderlyjc <ajacs1104@gmail.com>
2018-05-17 20:17:36 -0500

    Added sass support

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-
+import 'styles/_base.scss';
 
 const App = () => (
-    <h1>Hola Mundo Bitso Initial</h1>
+    <div className="main-page">
+        <h1>Hola Mundo Bitso Initial</h1>
+    </div>
 );
 export default App;
